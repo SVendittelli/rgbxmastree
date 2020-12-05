fab258e1f44a3a2c41c1060f99984093458321f2
Sam Vendittelli <sam.vendittelli@hotmail.com>
2020-12-05 14:10:49 +0000

    Add pattern list endpoint

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/patterns', methods=['GET'])
+def patterns():
+    patterns = []
+    for file in os.listdir('/home/pi/projects/rgbxmastree/patterns'):
+        patterns.append(file[:-3])
+    patterns.sort()
+    return jsonify(patterns)
